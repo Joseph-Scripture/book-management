f691effeb4108bd456ebee5066a248408aab3561
Abanda Glory <abandaglory91@gmail.com>
2025-08-15 00:02:04 +0100

    Copied the script from script.js to book-details.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,4 @@
-document.getElementById('borrow-btn').addEventListener('click', () => {
-    window.location.href = 'borrow-btn';
-});
 
-document.getElementById('reserve-btn').addEventListener('click', () => {
-    alert('Book reserved!🙂');
-});
 
 
 
