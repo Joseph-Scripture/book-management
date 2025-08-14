95d2967cb6f1912c04a6161c49465032fd9127ec
Abanda Glory <abandaglory91@gmail.com>
2025-08-14 23:50:28 +0100

    Created event handlers for borrow and reserve buttons in the book-details.js file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,12 @@
+document.getElementById('borrow-btn').addEventListener('click', () => {
+    window.location.href = 'borrow-btn';
+});
+
+document.getElementById('reserve-btn').addEventListener('click', () => {
+    alert('Book reserved!🙂');
+});
+
+
 
 
 
