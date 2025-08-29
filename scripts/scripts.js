449692b19bec7127926f748e2e06876fc3bff0fd
Joseph-Scripture <joescript237@gmail.com>
2025-08-29 11:31:53 +0100

    feat: Added verification  to password fields, corrected all file paths

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,30 @@ window.onload = function() {
 
     // 1. Array of image URLs
     const images = [
-        'url("images/home-page/1984.jpeg")',
-        'url("images/home-page/gatsby.jpeg")',
-        'url("images/home-page/obstacle.jpeg")',
+        // 'url("images/home-page/1984.jpeg")',
+        // 'url("images/home-page/gatsby.jpeg")',
+        // 'url("images/home-page/obstacle.jpeg")',
+        // 'url("images/home-page/next.avif")',
+        // 'url ("images/home-page/book.avif")',
+         'url("images/home-page/librarian.avif")',
+         'url("images/home-page/test.avif") ',
+         'url("images/home-page/photo-1664882338311-12c90557892d.avif")'
+        
     ];
 
-    // 2. Get the header element
     const header = document.getElementById('home');
+    const library = document.querySelector('.browse');
+    const action = document.querySelector('.action');
+
+    library.addEventListener('click', () => {
+        window.location.href = 'library.html';
+    });
+
+    action.addEventListener ('click', () =>{
+        window.location.href = 'login.html';
+    });
+    
+
 
     let currentIndex = 0;
 
